6c7efd8b52c5b5f11839c3045f6eb7631fde5ba8
Raihan Sidi <sidiraihan@gmail.com>
2022-11-05 23:47:22 +0700

    add info

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,16 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Domain For Sale</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Domain For Sale" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          To buy this domain please contact <b>sidiraihan@gmail.com</b>
         </p>
       </main>
-
-      <Footer />
     </div>
   )
 }
